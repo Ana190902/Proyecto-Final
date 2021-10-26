c4163cc2fe28778e221f3f246e56682483f51b20
Ana190902 <80015105+Ana190902@users.noreply.github.com>
2021-10-26 15:39:13 -0500

    Actualizado

--- mainwindow.h
+++ mainwindow.h
@@ -1,25 +1,40 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QMainWindow>
 #include <QGraphicsScene>
 #include <QGraphicsView>
 #include <QKeyEvent>
 #include <QWidget>
 #include <QList>
 #include <Player.h>
+#include <enemigos.h>
 
-class MainWindow: public QGraphicsView
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow: public QMainWindow
 {
 public: // Funciones
-    void keyPressEvent(QKeyEvent *event) override;
     void mousePressEvent(QMouseEvent *event) override;
     void mouseMoveEvent(QMouseEvent *event) override;
+    void keyPressEvent(QKeyEvent *event) override;
+    static MainWindow *getpointer();
+
     MainWindow(QWidget * = nullptr);
     ~MainWindow();
 
 
 public: // Variables
     Player* Player1;
+    Player* Player2;
     QGraphicsScene *Scene;
+    QGraphicsView *view;
+    Shuriken* Shuriken1;
+    Ui::MainWindow *ui;
+
+private:
+    static MainWindow* p_game;
 };
 #endif // MAINWINDOW_H
