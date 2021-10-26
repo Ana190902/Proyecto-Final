92c9a7e405c3e5384123fdc67315992a96fae176
Ana190902 <80015105+Ana190902@users.noreply.github.com>
2021-10-26 15:38:38 -0500

    Actualizado

--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    w.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     w.show();
     return a.exec();
 }
