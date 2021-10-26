7855622b4729e4f2479156dba78a278a0d7a0e80
Ana190902 <80015105+Ana190902@users.noreply.github.com>
2021-10-26 15:40:00 -0500

    Actualizado

--- municion.h
+++ municion.h
@@ -1,11 +1,31 @@
 #ifndef MUNICION_H
 #define MUNICION_H
+#include <QObject>
+#include <QGraphicsPixmapItem>
+#include <QTimer>
+#include <utilidades.h>
+#include <Player.h>
 
 
-class municion
+
+class municion :  public QObject,public IDs
 {
+Q_OBJECT
 public:
+    municion(double velocidadx,double velocidady, int ID1 ,QGraphicsItem* = nullptr);
     municion();
+    ~municion();
+
+    P::pair<double> getVelocidad() const;
+    void setVelocidad( const P::pair<double> &value);
+
+public slots:
+     void move();
+
+private:
+    QTimer *timet;
+    P::pair<double> velocidad;
+    uint llegada;
 };
 
 #endif // MUNICION_H
