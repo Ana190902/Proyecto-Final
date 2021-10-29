479d70c219386e0904aca292f72f5124ab693a70
Ana190902 <80015105+Ana190902@users.noreply.github.com>
2021-10-29 03:45:14 -0500

    Finalizado

---
+++ vidas.h
@@ -0,0 +1,11 @@
+#ifndef VIDAS_H
+#define VIDAS_H
+#include <QGraphicsItem>
+#include <utilidades.h>
+
+class Vidas : public IDs
+{
+public:
+    Vidas(int x,int y,QGraphicsItem* = nullptr);
+};
+#endif // VIDAS_H
