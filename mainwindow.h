b2459be089b75512102e9cc421d4af578b0be573
Ana190902 <80015105+Ana190902@users.noreply.github.com>
2021-10-29 03:40:55 -0500

    Finalizado

--- mainwindow.h
+++ mainwindow.h
@@ -7,8 +7,15 @@
 #include <QKeyEvent>
 #include <QWidget>
 #include <QList>
-#include <Player.h>
-#include <enemigos.h>
+
+#include "score.h"
+#include "vidas.h"
+#include "Player.h"
+#include "enemigos.h"
+#include "fmanager.h"
+#include "register.h"
+
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,24 +24,29 @@ QT_END_NAMESPACE
 class MainWindow: public QMainWindow
 {
 public: // Funciones
+    MainWindow(QWidget * = nullptr);
+    ~MainWindow() override;
+
     void mousePressEvent(QMouseEvent *event) override;
     void mouseMoveEvent(QMouseEvent *event) override;
     void keyPressEvent(QKeyEvent *event) override;
-    static MainWindow *getpointer();
-
-    MainWindow(QWidget * = nullptr);
-    ~MainWindow();
-
 
 public: // Variables
-    Player* Player1;
-    Player* Player2;
+    Vidas* vida;
+    Score* score;
+    Score* vidas;
     QGraphicsScene *Scene;
     QGraphicsView *view;
     Shuriken* Shuriken1;
+    QTimer* CollitionsTimer;
+    QList<Player *> Players;
+    QList<Ninja *> Enemies;
+    fmanager *Manager;
     Ui::MainWindow *ui;
+    Register *RLWindow;
+
 
 private:
-    static MainWindow* p_game;
+    //static MainWindow* p_game;
 };
 #endif // MAINWINDOW_H
